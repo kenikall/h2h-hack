499a3c1a54fdb74e1a93a0c8180af127b8b60a7c
Alexis Carroll <lex.ac1@gmail.com>
2017-01-28 13:51:59 -0800

    Adds embeded video with styling

--- app/assets/javascripts/quizzes.js
+++ app/assets/javascripts/quizzes.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
-  // meetingLinkGrabber();
-
+  var meetingURL = ""
+  var meetingId = ""
+  meetingLinkGrabber();
+  console.log(meetingURL)
 });
+
+var meetingFrame = function(){
+    $('#user4').click(function () {
+      event.preventDefault();
+      console.log('clicked the video listener')
+
+      document.getElementById('q-show-content').innerHTML='<object class="video" style="height:800px; width:1000px;" type="text/html" data=' + meetingURL + '></object>'
+    })
+}
+
 
 // Grabs meeting URL and meeting ID
 var meetingLinkGrabber = function() {
@@ -10,19 +22,25 @@ var meetingLinkGrabber = function() {
     method: "POST",
     headers: {"Content-Type": "application/json"},
     data: JSON.stringify({
+      "customLayouts":[
+        {"component": "string",
+        "width": 12,
+        "height": 12
+        }
+      ],
       "applicationId": "9155BF89-2C10-4E43-88F3-73748AC158D0",
       "name": "mannah",
       "templateId": "DA22B2BF-19FE-4463-8FBC-508683D4A76D"
-        })
+    })
   });
 
   request.done(function(response) {
     // console.log('response', response.data);
-    var meetingURL = response.data.meetingURL;
+    meetingURL = response.data.meetingURL;
     console.log('meetingURL', meetingURL);
-    var meetingId = response.data.meetingId;
+    meetingId = response.data.meetingId;
     console.log('meetingId', meetingId);
-
+    meetingFrame();
   });
 
   request.fail(function(response) {
