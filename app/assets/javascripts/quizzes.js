566c1e2938ac7c03cc7559d60f8ba8c1f8dfd4ad
reeses-pieces <nicholasreis@icloud.com>
2017-01-28 10:49:40 -0800

    add meeting link grabber function

--- app/assets/javascripts/quizzes.js
+++ app/assets/javascripts/quizzes.js
@@ -1,24 +1,31 @@
- $(document).ready(function() {
+$(document).ready(function() {
+  meetingLinkGrabber();
 
-  console.log("Grabs meeting link!!!!!!");
-    var request = $.ajax({
-      url: "https://sandbox.liveh2h.com/tutormeetweb/rest/v1/meetings/createInstantMeeting",
-      method: "POST",
-      headers: {"Content-Type": "application/json"},
-      data: JSON.stringify({
-        "applicationId": "9155BF89-2C10-4E43-88F3-73748AC158D0",
-        "name": "nick",
-        "templateId": "DA22B2BF-19FE-4463-8FBC-508683D4A76D"
-          })    
-    });
+});
 
-    request.done(function(response) {
-      console.log('response', response);
-      $("#api").append(response);
-
-    });
-
-    request.fail(function(response) {
-      alert("An error occured.");
-    });
+// Grabs meeting URL and meeting ID
+var meetingLinkGrabber = function() {
+  var request = $.ajax({
+    url: "https://sandbox.liveh2h.com/tutormeetweb/rest/v1/meetings/createInstantMeeting",
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    data: JSON.stringify({
+      "applicationId": "9155BF89-2C10-4E43-88F3-73748AC158D0",
+      "name": "mannah",
+      "templateId": "DA22B2BF-19FE-4463-8FBC-508683D4A76D"
+        })    
   });
+
+  request.done(function(response) {
+    // console.log('response', response.data);
+    var meetingURL = response.data.meetingURL;
+    console.log('meetingURL', meetingURL);
+    var meetingId = response.data.meetingId;
+    console.log('meetingId', meetingId);
+
+  });
+
+  request.fail(function(response) {
+    alert("An error occured.");
+  });
+};
